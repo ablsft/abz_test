d7bf0f83edfc8d8e90c9280df3c260c40ec4799b
Alexander Boriskin <iboriskin5@gmail.com>
2023-11-01 15:15:12 +0300

    Added models + migrations

--- employees_dir/models.py
+++ employees_dir/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Position(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название должности')
+    hierarchy_level = models.IntegerField(default=1, 
+                        validators=[MinValueValidator(1), MaxValueValidator(5)],
+                        verbose_name='Уровень иерархии')
+    
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=60, verbose_name='ФИО')
+    position = models.ForeignKey(Position, on_delete=models.CASCADE,
+                        verbose_name='Должность', related_name='employees')
+    employment_date = models.DateField(verbose_name='Дата приёма на работу')
+    salary = models.IntegerField(verbose_name='Размер заработной платы')
+    manager = models.ForeignKey('self', on_delete=models.CASCADE,
+                        verbose_name='Начальник', related_name='subordinates')
